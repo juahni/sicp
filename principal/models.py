7b477bb1ab3eba9f987ed84543c8cb299ee13b9c
usuario <usuario@usuario-OptiPlex-390>
2014-04-11 11:53:55 -0400

    Agregar al modelo la clase Usuario para completar la tabla por el Administrador

--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+# Importamos la tabla User.
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    """
+    Esta clase extiene el modelo User de django para agragarle
+    los campos que se necesitan en la base de datos.
+
+    """
+
+    # Campos agregados al modelo User.
+    ci = models.IntegerField(unique=True, null=True)
+    direccion = models.CharField(max_length=200, blank=True)
+    telefono = models.CharField(max_length=15, blank=True)
+    # Clave foranea unica al modelo User. Establece la relacion uno a uno.
+    user = models.ForeignKey(User, unique=True)
